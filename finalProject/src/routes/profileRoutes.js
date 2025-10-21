513772032944d7c2980adfd09b9c8fa2b5b05111
Aylin <aylinhanai12@gmail.com>
2025-10-21 16:05:55 +0300

    Add profile switching and redirect functionality

--- finalProject/src/routes/profileRoutes.js
+++ finalProject/src/routes/profileRoutes.js
@@ -10,4 +10,18 @@ router.put('/:profileId', isAuthenticated, updateProfileName);
 
 router.post('/', isAuthenticated, createNewProfile);
 
+router.post('/switch', isAuthenticated, (req, res) => {
+  const { profileId } = req.body || {};
+  if (!profileId) {
+    return res.status(400).send('profileId is required');
+  }
+
+  res.cookie('activeProfileId', profileId, {
+    httpOnly: false,
+    maxAge: 1000 * 60 * 60 * 24 * 30 
+  });
+
+  return res.redirect('/catalog'); 
+});
+
 module.exports = router;
