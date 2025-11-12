f673d6e6ff9e8dbb81a58242ca2e5788f9089e00
Yoav Slama <yoav.slama@gmail.com>
2025-11-13 00:10:06 +0200

    Updated populate script and changed "SuperNetflix" branding to "Streamix"

--- finalProject/src/utils/db.js
+++ finalProject/src/utils/db.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 async function connectToDatabase() {
 
-    const uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/SuperNetflix';
-    // mongoose.set('strictQuery', true); // this means we ignore unknown keys that doesn't exist in schema
+    const uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/streamix';
     try{
         await mongoose.connect(uri, {
             autoIndex: true,
